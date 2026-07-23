4a9231b6a1768188b098a106c63e56529dc5b80e
MadKloK <madklok12@gmail.com>
2026-07-23 12:37:14 +0330

    add task admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from tasks.models import Task
 
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'description', 'completed', 'created_at']
+    empty_value_display = '-empty-'
+    date_hierarchy = 'created_at'
+    search_fields = ['completed', 'user']
+    ordering = ['-created_at']
