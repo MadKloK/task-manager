a4f9244d5bcf85512314349751ed2c31e64441dc
MadKloK <madklok12@gmail.com>
2026-07-23 15:18:44 +0330

    add task list page

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from tasks.models import Task
 
 # Create your views here.
+
+@login_required
+def index_view(request):
+    tasks = Task.objects.filter(user=request.user)
+    context = {'tasks': tasks}
+    return render(request, 'tasks/tasks-index.html', context)
