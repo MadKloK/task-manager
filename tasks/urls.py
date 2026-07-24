ac4133af97776fee91cb05265fa16e2f0b13d5e0
MadKloK <madklok12@gmail.com>
2026-07-24 11:58:11 +0330

    add task creation form

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from tasks.views import index_view
+from tasks import views
 
 app_name = 'tasks'
 
 urlpatterns = [
-    path('', index_view, name='index')
+    path('', views.task_list_view, name='task-list'),
+    path('create/', views.task_create_view, name='task-create'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from tasks.models import Task
+from tasks.forms import TaskForm
 
 # Create your views here.
 
 @login_required
-def index_view(request):
+def task_list_view(request):
     tasks = Task.objects.filter(user=request.user)
     context = {'tasks': tasks}
-    return render(request, 'tasks/tasks-index.html', context)
+    return render(request, 'tasks/task_list.html', context)
+
+@login_required
+def task_create_view(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+
+            messages.success(request, 'Task created successfully.')
+            return redirect('tasks:task-list')
+
+        messages.error(request, 'Invalid Task.')
+
+    else:
+        form = TaskForm()
+
+    context = {'form': form}
+    return render(request, 'tasks/task_form.html', context)
