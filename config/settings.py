2ff71dcd0c4865fae58ba3577d42a6e6603b2154
MadKloK <madklok12@gmail.com>
2026-07-27 16:29:01 +0330

    Add enviornment confs

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +30,11 @@ DEBUG = config(
     cast=bool
 )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost",
+    cast=Csv()
+)
 
 
 # Application definition
